9bd819a292992a3d82dae213947e1bab60276e4b
Vinhdev04 <vinhnam04012004@gmail.com>
2024-04-30 22:13:03 +0700

    upload source code js-variable and built-in basic

---
+++ lesson_1/main.js
@@ -0,0 +1,37 @@
+// alert("Xin chao");
+// prompt("Enter name: ");
+
+
+var myHouse;
+let yourPlayer;
+
+var myHouse = "Ha Tinh";
+let myPlayer = "Ronaldo";
+const height = 40;
+
+console.log(myHouse);
+console.log(myPlayer);
+console.log("Height:"+" " +height + "myPlayer:" +" " +myPlayer);
+
+
+
+let yourName = prompt("M?i nh?p tên c? th?: ");
+console.log("Your name: " + yourName);
+console.log("Your name: " + yourName)
+console.log("Welcome " + yourName + " To " + "pcv.fed");
+
+var myFavorite = "Study";
+console.warn(myFavorite);
+console.error(myFavorite);
+console.log(myFavorite);
+console.info(myFavorite);
+
+confirm("Checked !");
+
+setTimeout(function () {
+    alert("Notification");
+},1000);
+
+setInterval(function () {
+    alert("Message" + Math.random());
+})
