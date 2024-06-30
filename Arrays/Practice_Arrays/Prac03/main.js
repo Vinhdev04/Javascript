0ce79c465fd92600e608b855d758c64ab7d5444b
Vinhdev04 <pcv.fed@gmail.com>
2024-06-30 16:01:51 +0700

    upload source code Practice Array P3

--- Arrays/Practice_Arrays/Prac03/main.js
+++ Arrays/Practice_Arrays/Prac03/main.js
@@ -17,3 +17,29 @@
   -> Sao chép mảng và làm phẳng
   -> Tạo một mảng nhiều chiều và sao chép mảng đó và làm phẳng thành mảng một chiều, sau đó in ra mảng kết quả.
 */
+
+let firstArr = [1, 2, 3, 4, 5];
+console.log(firstArr.unshift(-1)); // Output: length = 6
+console.log(firstArr); // Output: -1 1 2 3 4 5
+
+let lastArrr = [1, 2, 3, 4, 5, 6];
+console.log(lastArrr.shift()); // Output: delete[1]
+console.log(lastArrr); // Output: 2 3 4 5 6
+
+let matchArr1 = [1, 3, 5, 6];
+let matchArr2 = [0, 2, 4, 8];
+console.log(matchArr1.concat(matchArr2)); // Output: 1 3 5 6 0 2 4 8
+
+let copyArr = [0, 3, 5, 8, 9];
+let copyArray = copyArr.slice();
+let insertArr = copyArr.unshift(1);
+console.log(copyArr); // Output: 1 0 3 5 8 9
+console.log(copyArray); // Output: 0 3 5 8 9
+
+let multiArr = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+];
+console.log(multiArr); // Output: [1,2] [3,4] [5,6]
+console.log(multiArr.flat()); // Output: 1 2 3 4 5 6
